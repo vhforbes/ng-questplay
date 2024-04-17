81c12c3a344d0f9569492e8682684dc0213a5616
Victor Forbes <vhforbes@gmail.com>
2024-04-16 21:12:22 -0300

    #setting-up

--- campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
+++ campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
@@ -5,6 +5,6 @@ contract HelloGuardian {
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
         // UNCOMMENT THE NEXT LINE TO MAKE THE TEST PASS
-        // return "Hello Guardian";
+        return "Hello Guardian";
     }
 }
