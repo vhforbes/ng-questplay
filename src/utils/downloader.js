6244efbfb87ce2a5493d53f9728650188a9b40de
Victor Forbes <39014716+vhforbes@users.noreply.github.com>
2024-04-16 21:04:43 -0300

    Initial commit

---
+++ src/utils/downloader.js
@@ -0,0 +1,111 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+
+import JSZip from 'jszip';
+import { Octokit } from '@octokit/rest';
+import { ProgressBar } from './progressbar.js'
+
+export class QuestDownloader {
+
+  constructor(options = {}) {
+    this.progressBar = new ProgressBar();
+    this._octokit = new Octokit(options.github);
+  }
+
+  async downloadQuest(owner, repo, directoryPath, options = {}) {
+    const zipFilePath = `${directoryPath}.zip`;
+
+    this.progressBar.start();
+    try {
+
+      const file = await this._octokit.repos.getContent({
+        owner,
+        repo,
+        ref: options.sha,
+        path: zipFilePath,
+      });
+  
+      const decodedContent = Buffer.from(
+        file.data.content, 
+        file.data.encoding
+      );
+
+      await this.unzip(decodedContent);
+
+    } catch (err) {
+      this.progressBar.fail("Download failed");
+      throw err;
+    }
+
+    await this.progressBar.stop("Download finished");
+  }
+
+  async downloadQuestplay(options = {}) {
+    this.progressBar.start();
+    try {
+
+      const zipped = await this._octokit.repos.downloadZipballArchive({
+        owner: "Nodeguardians",
+        repo: "ng-questplay",
+        ref: options.sha
+      });
+
+      await this.unzip(zipped.data, 1);
+
+    } catch (err) {
+      this.progressBar.fail("Download failed");
+      throw err;
+    }
+    await this.progressBar.stop("Download finished");
+  }
+
+  async unzip(data, depth = 0) {
+    const zip = await JSZip.loadAsync(data);
+
+    for (const key of Object.keys(zip.files)) {
+      const file = zip.files[key];
+
+      if (file.dir) {
+        continue;
+      }
+
+      if (depth > 0) {
+        file.name = file.name.split('/').slice(depth).join('/');
+      }
+      
+      var dirname = path.dirname(file.name);
+      if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname, { recursive: true });
+      }
+
+      fs.writeFileSync(file.name, await file.async("nodebuffer"));
+    }
+  }
+
+  async downloadFile(owner, repo, filePath, options = {}) {
+
+    const rootPath = options.rootPath == undefined
+      ? cwd()
+      : options.rootPath;
+
+    const file = await this._octokit.repos.getContent({
+      owner,
+      repo,
+      ref: options.sha,
+      path: filePath,
+    });
+
+    const decodedContent = Buffer.from(file.data.content, file.data.encoding);
+    fs.writeFileSync(path.join(rootPath, filePath), decodedContent);
+  }
+
+  async installSubpackage() {
+    await this.progressBar.start()
+      .then(() => child_process.execSync('npm install'))
+      .then(() => this.progressBar.stop("Installation finished"));
+  }
+
+}
