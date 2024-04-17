037096c78600718d0bb2d4ee205a4a49d6fabd98
Victor Forbes <vhforbes@gmail.com>
2024-04-16 21:10:14 -0300

    feat: setting up

--- campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
+++ campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.19;
 
 contract HelloGuardian {
-
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
         // UNCOMMENT THE NEXT LINE TO MAKE THE TEST PASS
-        // return "Hello Guardian";
+        return "Hello Guardian";
     }
-
 }
